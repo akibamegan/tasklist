2e30d918712e5345406e20dc22ce4901f1df8444
Megan Akiba <megan.akiba@colorado.edu>
2019-12-11 23:24:07 -0700

    Updates

---
+++ precache-manifest.dddb35d302fbc641d6d38171b68b302d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a2320f4ca9d34ba0f19d8ee7913fca",
+    "url": "/tasklist/index.html"
+  },
+  {
+    "revision": "fb690c98ce967731e89e",
+    "url": "/tasklist/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7510195781815229ad67",
+    "url": "/tasklist/static/css/main.7c0e3cab.chunk.css"
+  },
+  {
+    "revision": "fb690c98ce967731e89e",
+    "url": "/tasklist/static/js/2.18e5d979.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tasklist/static/js/2.18e5d979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7510195781815229ad67",
+    "url": "/tasklist/static/js/main.863d78ab.chunk.js"
+  },
+  {
+    "revision": "fd2942b04a12a5cdd592",
+    "url": "/tasklist/static/js/runtime-main.2d7b5025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tasklist/static/media/logo.5d5d9eef.svg"
+  }
+]);
